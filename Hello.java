41ddc611603b0d1e6b425d7c1248dea01855ee26
Scotty <40522299+Jack-O-Brian@users.noreply.github.com>
2022-02-10 15:14:28 -0500

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,6 +1,7 @@
+// Your First Program
 
-abcdefghijklmnopqwe
-
-abcdefghijklmnopqwe
-
-abcdefghijklmnopqwe
+class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
+}
